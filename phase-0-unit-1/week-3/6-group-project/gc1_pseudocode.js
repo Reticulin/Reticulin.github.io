4454b4e06640a2a4743af97ffb0227fc65afc541
Joseph Sabato <pollenachoo@gmail.com>
2014-07-13 18:01:42 -0700

    finished week 3

--- phase-0-unit-1/week-3/6-group-project/gc1_pseudocode.js
+++ phase-0-unit-1/week-3/6-group-project/gc1_pseudocode.js
@@ -5,3 +5,6 @@
 create a function that adds the numbers in each list and presents it as "sum"
 create a function that takes the numbers in each list and finds the average and presents it as the "mean"
 create a function that finds the middle value in each list and presents it as the "median"
+
+//Reflection
+/*This was a somewhat scary experience. I needed to make sure that what I had written would be easily understandable to my group members. My group was successful in passing the tests. What I learned from this excercise is that communication between your fellow coders is crucial to success. If you cannot communicate, then things will get jumbled and there will be problems. Make sure your clear and specific with stories and pseudocode to make it easier for others to understand and pick up where you left off quickly.
