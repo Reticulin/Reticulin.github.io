8a77ae2034941304323382dc20fa9d961fb0eb01
Joseph Sabato <pollenachoo@gmail.com>
2014-07-19 11:02:37 -0700

    working on w4a5

--- phase-0-unit-2/week-4/5-pad-array/my_solution.rb
+++ phase-0-unit-2/week-4/5-pad-array/my_solution.rb
@@ -14,8 +14,20 @@
 
 
 # 2. Initial Solution
-
-
+class Array
+  def pad!(min_size, value = nil)
+    x = min_size - self.count #take the argument and subtract the count from it.
+    x.times do
+     self << value
+    end# I want to add the value x number of times to the array
+    
+    self
+  end
+  
+  def pad(min_size, value = nil)
+  self.clone.pad!(min_size, value)
+  end
+end
 
 
 # 3. Refactored Solution
