7348b83b3fcc1d4b6855bc35ecc45aa693fb57f1
Joseph Sabato <pollenachoo@gmail.com>
2014-07-22 08:20:35 -0700

    finished w5a1 and a2

--- phase-0-unit-2/week-5/1-die/my_solution.rb
+++ phase-0-unit-2/week-5/1-die/my_solution.rb
@@ -5,41 +5,65 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: sides of the die
+# Output: sides and random number from rolling the die
+# Steps: Have a class with attributes and methods. Create a statement that gives an error when the sides of the die are less than 1. define the sides method and define the roll method to give a random number.
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+  	@sides = sides
+    unless (sides >= 1)
+    	raise ArgumentError
+    end
   end
-  
   def sides
-    # code goes here
+  	return @sides
   end
-  
   def roll
-    # code goes here
+    rand(1..sides)
   end
 end
 
 
 
-# 4. Refactored Solution
 
+# 4. Refactored Solution
+class Die
+  def initialize(sides)
+  	@sides = sides
+    unless (sides >= 1)
+    	raise ArgumentError
+    end
+  end
+  
+  def sides
+  	return @sides
+  end
+  
+  def roll
+    rand(1..sides)
+  end
+end
 
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+sides = rand(800)
+die = Die.new(sides)
+die.sides == sides
 
+(Die.instance_method(:sides).arity) == 0
+
+(Die.instance_method(:roll).arity) == 0
 
 
 
 
 
 # 5. Reflection 
+#My strategy here was to break up what I needed to do in order to focus on one part at a time. After breaking up each part, I test them separately to see if they work before moving onto the next part. It was a fun assignment, but one part got frustrating due to me trying to find something that I was missing. Once I found it (simple issue), everything worked like it should.
