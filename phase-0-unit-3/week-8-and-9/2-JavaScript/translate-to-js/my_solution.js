042d9a33df15c9c54f454f6e8809364442a62ea8
Reticulin <Reticulin@Josephs-MacBook-Air.local>
2014-08-22 15:30:12 -0700

    finished more review

--- phase-0-unit-3/week-8-and-9/2-JavaScript/translate-to-js/my_solution.js
+++ phase-0-unit-3/week-8-and-9/2-JavaScript/translate-to-js/my_solution.js
@@ -1,29 +1,101 @@
 // U3.W8-9: Challenge you're converting
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge [by myself, with:Kevin Aungle ].
 
 // 2. Pseudocode
-
+//create main function for credit card
+//create functions for check, double and sum
 
 
 // 3. Initial Solution
+/*function CreditCard(number) {
+	this.number = number;
+	digits = [];
+	sNumber = number.toString();
+	for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    digits.push(+sNumber.charAt(i));
+}
+	if (digits.length != 16) {
+		throw new Error("Invalid Number");
+	}
+}
 
+CreditCard.prototype.check = function(number) {
+	number = this.number;
+	digits = [];
+	ddigits = [];
+	sNumber = number.toString();
+	for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    digits.push(+sNumber.charAt(i));
+}
+
+	for (var i = 1; i <= digits.length; i++) {
+		if (i % 2 === 0) {
+			ddigits.push(i*2);
+		}
+		else {
+			ddigits.push(i);
+		}
+	}
+	for (var i = 0, sum = 0; i < ddigits.length; sum += ddigits[i++]);
+	if (sum%10===0){
+		console.log(true);
+	}
+	else {
+		console.log(false);
+	}
+}
+*/
 
 
 
 // 4. Refactored Solution
+function CreditCard(number){
+	this.number = number;
+	digits = [];
+	ddigits = [];
+	sNumber = number.toString();
+	for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    digits.push(+sNumber.charAt(i));
+}
+	if (digits.length != 16) {
+		throw new Error("Invalid Number");
+	}
+	for (var i = 0; i < digits.length; i++) {
+		if (i % 2 === 0) {
+			ddigits.push(digits[i]*2);
+		}
+		else {
+			ddigits.push(digits[i]);
+		}
+	}
+	together = ddigits.join('');
+	intnum = together.split('');
+	sdigits = [];
 
+	for (var i = 0; i < intnum.length; i++){
+    sdigits.push(+intnum[i])
+}
+	for (var i = 0, sum = 0; i < sdigits.length; sum += sdigits[i++]);
+	if (sum%10===0){
+		console.log(true);
+	}
+	else {
+		console.log(false);
+	}
+}
 
 
 
 
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
-
-
+CreditCard(1234567890123456)//should be false
+CreditCard(4408041234567893)//should be true
 
 
 
 
 // 5. Reflection 
+//Our strategy going into this assignment was to try and mimic our class on the validate credit card assignment. It was tough since JS doesn't have classes, so we made functions. We later realised we could condense into one function.
--- phase-0-unit-3/week-8-and-9/3-Ruby/what-the-scope/my_solution.rb
+++ phase-0-unit-3/week-8-and-9/3-Ruby/what-the-scope/my_solution.rb
@@ -6,42 +6,69 @@
 
 
 # Original Code
+#
+#$var_1 = 22
+# 
+#class Person
+#  @@var_2
+#  VAR_6 = "Ruby"
+# 
+#  attr_reader :var_3, :var_4
+# 
+#  def initialize(var_5 = VAR_6)
+#    @var_3="Law of Demeter"  
+#  end
+# 
+#  def do_stuff(var_7=[1,2,3])
+#    var_7.each do |var_8|
+#      var_9 += var_8 + 2
+#    end
+#  end
+#end
 
-$var_1 = 22
- 
+# 1. Modified Code (with scope identified as comments)
+$var_1 = 22 #global variable that stays constant throughout the entire ruby program
+
 class Person
-  @@var_2
-  VAR_6 = "Ruby"
- 
-  attr_reader :var_3, :var_4
- 
-  def initialize(var_5 = VAR_6)
-    @var_3="Law of Demeter"  
+  @@var_2 #class variable that is shared among all instances of the class, in this case, the Person class
+  VAR_6 = "Ruby" # a local variable which can't be accessed outside this code of construct
+  attr_reader :var_3, :var_4#variables set as readers in order to call them outside the object
+
+  def initialize(var_5)#local variable for the initialize method
+    var_5 = VAR_6 #since VAR_6 was a local variable at the beginning of the object, it is not defined for this method
+    var_3 = "Law of Demeter"#local variable that can only be called within this method
   end
- 
-  def do_stuff(var_7=[1,2,3])
-    var_7.each do |var_8|
-      var_9 += var_8 + 2
+
+  def do_stuff(var_7)#local variable for the do_stuff method
+    var_7 = [1,2,3] #setting an array of numbers to the local variable var_7
+    var_7.each do |var_8|#looping through var_7 which will output each number which is symbolized by var_8
+      var_9 = var_8 + 2 #var_9 gets the output of the previous loop plus 2
     end
   end
 end
-
-# 1. Modified Code (with scope identified as comments)
-
 
 
 
 
 # 0 & 3. Call the methods here. Later, come back and add tests
-
+x = Person.new(5)
+x.do_stuff(5)
 
 # What are the problems in the error messages?
 
 
 
 # 2. What is the scope of each variable?
-
-
+#$var_1 is a global variable
+#@@var_2 is a class variable
+#var_3 is an instance variable
+#var_4 is currently only set as an attr_reader, but would probably be an instance variable just like var_3
+#var_5 is a local variable
+#var_6 is a local variable twice
+#var_7 is a local variable
+#var_8 is a local variable
+#var_9 is a local variable
 
 
 # 4. Reflection 
+#Seemed challenging at first, but I was able to get it working. I never really appreciated user stories until today. It's hrd to try and fix a code without knowing what purpose it serves.
